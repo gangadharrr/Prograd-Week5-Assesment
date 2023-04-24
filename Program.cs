640c6d4ad25b0c2d441fc5b65167939ba8a0e54f
Gangadhar C <gangadharganga90@gmail.com>
2023-04-24 12:55:50 +0530

    Question 2

--- Program.cs
+++ Program.cs
@@ -2,28 +2,58 @@
 namespace Prograd__Week5_Assesment
 {
     /*
-     *Q1
-        Password Validation
-        Create a function that validates a password to conform using c#.net to the following rules:
-        Length between 6 and 24 characters.
-        At least one uppercase letter (A-Z).
-        At least one lowercase letter (a-z).
-        At least one digit (0-9).
-        Maximum of 2 repeated characters.
-        "aa" is OK
-        "aaa" is NOT OK
-        Supported special characters:
-        ! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; " ' ? < > , .
+     Q2
+        For this exercise, keep in mind the following definitions:
+        A term is either an initials or word.
+        initials = 1 character
+        words = 2+ characters (no dots allowed)
+        A valid name is a name written in one of the following ways:
+        H. Wells
+        H. G. Wells
+        Herbert G. Wells
+        Herbert George Wells
+        The following names are invalid:
+        Herbert or Wells (single names not allowed)
+        H Wells or H. G Wells (initials must end with dot)
+        h. Wells or H. wells or h. g. Wells (incorrect capitalization)
+        H. George Wells (middle name expanded, while first still left as initial)
+        H. G. W. (last name is not a word)
+        Herb. G. Wells (dot only allowed after initial, not word)
+        Rules
+        Both initials and words must be capitalized.
+        Initials must end with a dot.
+        A name must be either 2 or 3 terms long.
+        If the name is 3 words long, you can expand the first and middle name or expand the first name only. You cannot keep the first name as an initial and expand the middle name only.
+        The last name must be a word (not an initial).
+        Your task is to write a function that determines whether a name is valid or not. Return true if the name is valid, false otherwise.
         Examples
-        ValidatePassword("P1zz@") ➞ false
-        // Too short.
-        ValidatePassword("Fhg93@") ➞ true
-        // OK!
+        ValidName("H. Wells") ➞ true
+        ValidName("H. G. Wells") ➞ true
+        ValidName("Herbert G. Wells") ➞ true
+        ValidName("Herbert") ➞ false
+        // Must be 2 or 3 words
+        ValidName("h. Wells") ➞ false
+        // Incorrect capitalization
+        ValidName("H Wells") ➞ false
+        // Missing dot after initial
+        ValidName("H. George Wells") ➞ false
+        // Cannot have: initial first name + word middle name
+        ValidName("H. George W.") ➞ false
+        // Last name cannot be initial
+        ValidName("Herb. George Wells") ➞ false
+        // Words cannot end with a dot (only initials can)
+        Notes
+        A term is either an initial or word.
+        Initials and words are disjoint sets.
      */
-    internal class Program
+    public class Program
     {
-        
-        static bool PasswordValidation(string Password)
+        public static bool NameValidation(string Name)
+        {
+            Regex re = new Regex("^(?:([A-Z]{1}[a-z]+\\s)+([A-Z]\\.\\s)*|([A-Z]\\.\\s)+)([A-Z]{1}[a-z]+)$");
+            return re.IsMatch(Name);
+        }
+        public static bool PasswordValidation(string Password)
         {
             bool IsPasswordValid = true;
             if (Password.Length >= 6 && Password.Length <= 24)
@@ -61,10 +91,10 @@ namespace Prograd__Week5_Assesment
             else { IsPasswordValid = false; }
             return IsPasswordValid;
         }
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string Password = Convert.ToString(Console.ReadLine());
-            Console.WriteLine(PasswordValidation(Password));
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(NameValidation(name));
         }
     }
 }
